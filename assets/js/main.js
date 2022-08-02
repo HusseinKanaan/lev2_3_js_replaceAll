454e0bf09ee048fe63d98f7b3671a9d2b568fca8
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-03 00:19:30 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-let search = document.getElementById("search-input")
-
-console.log(search)
 
 function searching() {
-    
+    let searchInput = document.getElementById("search-input").value;
+    let artikelText = document.getElementById("artikelText1");
+
+    searchInput = searchInput.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+    let pattern = new RegExp(`${searchInput}`, "gi");
+
+    artikelText.innerHTML = artikelText.textContent.replace(pattern, match => `<mark>${match}</mark>`);
+
+
 }
